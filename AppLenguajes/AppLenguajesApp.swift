1702f7bae0b24a59784dcb87ed6f5bd2b3e5c4de
Francisco Guerra <paco.gq.0709@gmail.com>
2022-09-27 14:27:49 -0500

    Initial Commit

---
+++ AppLenguajes/AppLenguajesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLenguajesApp.swift
+//  AppLenguajes
+//
+//  Created by Paco Guerra on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct AppLenguajesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
